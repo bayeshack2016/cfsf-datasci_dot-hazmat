669167dcf73a5b84249e2275064193ffcff42288
Jude C <jude@ucla.edu>
2016-07-26 10:58:15 -0700

    Latest Update to Shiny Anomaly Detector
    Getting there...
    * UI's tableOutput now working and soon to be reactive.
    * SERVER's Leaflet map rendering now working, with variable polygon
    opacity, and soon to be reactive.
    * Need to make both of the above reactive to user's month selection.
    * Need to make state-specific incident timeline and summary (bottom
    columns) reactive to user's Leaflat map clicks.

--- shiny_anomaly_detection/README.md
+++ shiny_anomaly_detection/README.md
@@ -3,11 +3,19 @@ by [Jude Calvillo](http://linkd.in/vVlpXA)
 [Data Science Working Group](http://datascience.codeforsanfrancisco.org)  
 [Code for San Francisco](http://www.codeforsanfrancisco.org)  
 
-This is nowhere near complete, but in case my Data Science Working Group, Code for SF, and/or Bayes Hack friends would ocassionally like to see my progress, go here:  
+This isn't done yet, but in case my Data Science Working Group, Code for SF, and/or Bayes Hack friends would ocassionally like to see my progress, go here:  
 
 ### [Shiny + R DoT Anomaly Detector >>](https://judec.shinyapps.io/shiny_anomaly_detection/)
 
-Much more to come! Stay tuned...
+### Latest Update: 7/26/16  
 
-Last update: 7/24/16  
+*v1.12 - Starting to add functionality and interactivity*  
 
+* UI's tableOutput now working and soon to be reactive
+* SERVER's Leaflet map rendering now working, with variable polygon opacity, and soon to be reactive
+* Need to make both of the above reactive to user's month selection
+* Need to make state-specific incident timeline and summary (bottom columns) reactive to user's Leaflat map clicks
+
+#### SEO tags, because, well, why not? :)
+
+Shiny, Shiny app, anomaly detection, Leaflet, department of transportation, data science working group, code for san francisco, R, r programming, jude calvillo
--- shiny_anomaly_detection/server.R
+++ shiny_anomaly_detection/server.R
@@ -1,13 +1,15 @@
 ## Shiny + R Anomaly App for the U.S. Dept. of Transportation
 ## By Jude Calvillo (Data Science Working Group @ Code for San Francisco)
 ##
-## Status: July 23, 2016:
+## Status: July 26, 2016:
 ## ---------------------
-## v1.1 - Just initiating app dev
+## v1.12 - Starting to add functionality and interactivity
 ##
-## - Initiating server and UI R scripts.
-## - Need to conceptualize UI and immediately deliverable features before continuing w/UI.
-##   I should be doing this over the weekend.
+## - UI's tableOutput now working and soon to be reactive
+## - SERVER's Leaflet map rendering now working, with variable polygon opacity, and soon to be reactive
+## - Need to make both of the above reactive to user's month selection
+## - Need to make state-specific incident timeline and summary (bottom columns) reactive to user's 
+##   Leaflat map clicks
 ##
 ## ----------------------------------
 ##
@@ -17,6 +19,8 @@ library(shiny)
 library(AnomalyDetection)
 library(leaflet)
 library(maps)
+
+## Get the data (loading it later for faster initial load)
 dat <- read.csv("data/hazmat_year_month.csv")
 
 ## Factorize state
@@ -32,30 +36,31 @@ res_df <- data.frame()
 ## Run each state's data through anomaly detection
 for(i in 1:length(levels(dat$State))){
     res <- AnomalyDetectionTs(dat[dat$State %in% levels(dat$State)[i],c("Year.Month","Report.Number")], max_anoms=0.05, 
-                              direction='both', plot=T)
+                              direction='pos', plot=T)
     per_state <- res$anom
     per_state$state <- rep(levels(dat$State)[i], nrow(per_state)) # Quick and dirty solution here.
     res_df <- rbind(res_df, per_state)
 }
 
 ## List anomolous states
-names(res_df)[2] <- c("incidents")
-res_df <- head(res_df[c(3,1,2)]) # just for testing
+names(res_df)[2:3] <- c("Incidents","State")
+res_df2 <- head(res_df[c(3,2)]) # just for testing
 
 ## Select and render the map
 mapStates = map("state", fill = TRUE, plot = FALSE)
 the_map <- leaflet(data = mapStates) %>% addTiles() %>%
-    addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+    addPolygons(fillColor = "#cc0000", fillOpacity = res_df2$Incidents * .01, stroke = FALSE)
 
 ## Initiate Shiny Server instance.
 shinyServer(
     function(input, output){
-    
+        
         ## Simply show the user the value they chose.
-        output$selectMONTH <- renderText({paste("Month Selected:", input$selectdate)})
+        output$selectMONTH <- renderText({paste("For", months(as.Date(input$selectdate)), 
+                                                        format(input$selectdate, "%Y"))})
         
         ## Show anomolous states (preview / proof of concept)
-        output$anonSTATES <- renderTable({res_df})
+        output$anonSTATES <- renderTable({res_df2})
         
         ## Show map
         output$theMAP <- renderLeaflet({the_map})
