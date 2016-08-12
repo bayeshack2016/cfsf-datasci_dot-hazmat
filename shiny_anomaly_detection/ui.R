45cb049fc8e649783b87dc771d3d71bc265f98e2
Jude C <jude@ucla.edu>
2016-08-11 20:53:59 -0700

    90% done! - Map interactivity complete
    * About 90% done now! Map interactivity complete, thereby updating
    anomaly plotting and UI.
    * Just need to integrate a news feed drawn from an API query (e.g.
    "hazmat incident/accident" + selected state + selected month).
    * Try it for yourself. :) Just remember that our dataset currently only
    goes up to Feb, 2016, and that not all months have anomalous states (but
    most months in 2013-2014 have at least one). Thanks!

--- shiny_anomaly_detection/ui.R
+++ shiny_anomaly_detection/ui.R
@@ -1,23 +1,13 @@
 ## Shiny + R Anomaly App for the U.S. Dept. of Transportation
 ## By Jude Calvillo (Data Science Working Group @ Code for San Francisco)
 ##
-## Status: August 10, 2016:
+## Status: August 11, 2016:
 ## ---------------------
-## v1.5 - Leaflet/map plotting issues fixed. Just need to add map interactivity now (and news!)
+## v1.6 - Just news left.
 ##
-## - UI's tableOutput now reactive and includes summary stats.
-## - I'd like to now include a news feed in the area formerly dedicated to summary stats, one that shows 
-##   hazmat-related news stories from the anomolous states for the selected month. :)
-## - Applied a bootstrap theme to pretty things up a bit, BUT it's hurt the readability of the date input.
-## - Apparently, there's no such thing as a purely by "month" date input widget, which makes date selection 
-##   a little confusing. For now, any date selected gets converted to the first of that month.
-## - Anomolous state-specific and date-range specific timeline plot, with anomalies highlighted, 
-##   now reactive to user input.
-## - Leaflet map rendering and anomalous state filling now working properly (yay!).
-## - Need to 'interact' with user's Leaflet map selection for specifying state to plot and summarize.
-## - Using state.abb for testing reactive input to state specific anomaly detection and plotting, BUT
-##   there seems to be a bug: only some states produce a plot. Hopefully, that's just due to some mismatch
-##   between the state.abb values and the State values in dat/dat2 (main data frames).
+## - About 90% done now! Map interactivity complete, thereby updating anomaly plotting and UI.
+## - Just need to integrate a news feed drawn from an API query (e.g. "hazmat incident/accident"
+##   + selected state + selected month).
 ##
 ## ----------------------------------
 ##
@@ -49,19 +39,22 @@ sidebarPanel(
               startview = 'year'),
     h2(" "),
     h4(icon("truck", lib="font-awesome"),"Anomalous States"),
-    tableOutput("anonSTATES"),
-    selectInput('selectstate', label = "Select State (test)", choices = state.abb) # temporary, for testing
+    tableOutput("anonSTATES")
     ),
 mainPanel(
     h3(style="color:#000000;", icon("map-o", lib="font-awesome"),"Anomalous States Heatmap"),
     leafletOutput("theMAP"),
     h2(" "),
-    column(6, h4(style="color:#000000;", icon("clock-o", lib="font-awesome"),"Incident Timeline for State (Month - 5yrs)"),
+    column(6, uiOutput("plotHEAD"),
             plotOutput("plotANOM")),
-    column(6, h4(style="color:#000000;", icon("info", lib="font-awesome"),"Hazmat News for Selected State + Month"),
-            h2(" "), 
-            p(style="color:#000000;", "This area will soon display a feed of hazmat-related news from the selected state and for the given month. This should provide explanatory context.")),
-    column(12, "Developed by", a("Jude Calvillo", href="http://linkd.in/vVlpXA"), "-", a("Data Science Working Group @ Code for San Francisco", href="http://datascience.codeforsanfrancisco.org"),
+    column(6, uiOutput("newsHEAD"),
+           h2(" "),
+           h2(" "),
+           p("Awaiting state selection...")
+            # includeHTML("www/rss-feed_sample.html")
+           ),
+    column(12, "Developed by", a("Jude Calvillo", href="http://linkd.in/vVlpXA"), "-", a("Data Science Working Group @ Code for San Francisco", 
+                                                                                         href="http://datascience.codeforsanfrancisco.org"),
             h2(" "),
             h2(" "))
     )
